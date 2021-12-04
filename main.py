c890c838ca9150e04b0ded39c612d86c4beeb332
gll19920817 <gll19920817@gmail.com>
2021-12-04 11:22:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import threading
 from urllib.parse import urlparse
 
 from scripts import watcher, webdriver
+from config.helper import config
 
 if __name__ == '__main__':
     if len(sys.argv) == 1 or not urlparse(sys.argv[1]).scheme:
